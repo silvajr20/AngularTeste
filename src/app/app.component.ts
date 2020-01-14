772b106b48b5d790ad45d6a96acef740e47cb66e
Silvano Jr <silvano17@live.com>
2020-01-14 10:30:56 -0300

    First commity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from './model/todo.model';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularTeste';
+  
+  public todos : Todo[] = [];
+  public title: String = 'Titulo legalzinho para a aplicação';
+  public form: FormGroup;
+  
+
+  constructor(private fb : FormBuilder){
+    
+    this.form = this.fb.group({
+      title : ['', Validators.compose([
+      Validators.minLength(3),
+      Validators.maxLength(60),
+      Validators.required
+
+    ])]    
+    });
+    
+    //this.todos.push(new Todo(1, 'Era para ser um teste', false));
+    //this.todos.push(new Todo(2, 'Era para ser um teste do teste que ja foi criado', false));
+    //this.todos.push(new Todo(3, 'Este é outro teste dos testes que ja foram criados', true));
+  }
+  
+ add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+ }
+
+ save(){
+   const data = JSON.stringify(this.todos);
+   localStorage.setItem('todos', data);
+ }
+
+ load(){
+    
+ }
+
+
+ clear(){
+   this.form.reset();
+ }
+  
+  remove(todo : Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUnDone(todo: Todo){
+    todo.done = false;
+  }
+
+
+
 }
